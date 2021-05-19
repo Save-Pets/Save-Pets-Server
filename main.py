503bb8d3ecc8ce9f9ce12710e5cc3b82d6efec01
MoonSueLym <msr56501354@gmail.com>
2021-05-19 15:58:44 +0900

    heroku

--- main.py
+++ main.py
@@ -272,4 +272,5 @@ def getSVMResultForRegister():
 #     return jsonify({'message':'fail'})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
